7cd79bc1f78e581e5c639c1e0b97e538f7f6f6ef
Zane Campbell <zanegitinit@gmail.com>
2018-12-16 11:56:41 -0800

    setup base layout for CookBookPage with material ui

--- src/components/CookBookPage.js
+++ src/components/CookBookPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RecipeList from './RecipeList';
+import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   buttonLabel: {
     fontSize: '20px'
+  },
+  paperWrapper: {
+    //boxShadow: 'none'
+    paddingTop: '30px',
+    textAlign: 'center',
+    backgroundColor: '#e6e6e6'
   }
 });
 
@@ -14,12 +21,12 @@ const CookBookPage = (props) => {
   const { classes } = props;
 
   return (
-  <div>
+  <Paper className={classes.paperWrapper}>
     <Link style={{ textDecoration: 'none' }} to='/create'>
-      <Button classes={{ label: classes.buttonLabel }}>Add Recipe</Button>
+      <Button style={{ backgroundColor: '#3f51b5', padding: '20px', marginBottom: '20px' }} classes={{ label: classes.buttonLabel }}>Add New Recipe</Button>
     </Link>
     <RecipeList />
-  </div>
+  </Paper>
   )
 }
 
