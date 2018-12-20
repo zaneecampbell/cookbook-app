72d008aef78e68ff00c839f6a23b37ab7fe7b7db
Zane Campbell <zanegitinit@gmail.com>
2018-12-19 18:26:30 -0800

    connected ViewRecipePage to redux and setup basic layout

--- src/components/ViewRecipePage.js
+++ src/components/ViewRecipePage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+
+});
 
 export const ViewRecipePage = (props) => {
+    const { classes } = props;
 
     return (
-        <div>hi</div>
+        <div>
+            <div>{props.recipe.id}</div>
+            <div>{props.recipe.name}</div>
+            <div>{props.recipe.ingredients}</div>
+            <div>{props.recipe.instructions}</div>
+            <div>{props.recipe.tags}</div>
+        </div>
     )
 };
 
-export default ViewRecipePage;
+const mapStateToProps = (state, props) => ({
+    recipe: state.recipes.find((recipe) => recipe.id === props.match.params.id)
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(ViewRecipePage));
