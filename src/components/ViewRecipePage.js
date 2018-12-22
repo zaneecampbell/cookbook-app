45768fae72b7b7f23015169836947af3539dc94b
Zane Campbell <zanegitinit@gmail.com>
2018-12-21 19:19:06 -0800

    started formatting for ViewResultsPage

--- src/components/ViewRecipePage.js
+++ src/components/ViewRecipePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
+    paperWrapper: {
+        maxWidth: '960px',
+        margin: 'auto',
+        paddingLeft: '20px',
+        paddingTop: '20px'
+    },
+    gridTitle: {
 
+    }
 });
 
 export const ViewRecipePage = (props) => {
@@ -11,11 +22,23 @@ export const ViewRecipePage = (props) => {
 
     return (
         <div>
-            <div>{props.recipe.id}</div>
-            <div>{props.recipe.name}</div>
-            <div>{props.recipe.ingredients}</div>
-            <div>{props.recipe.instructions}</div>
-            <div>{props.recipe.tags}</div>
+            <Paper classes={{ root: classes.paperWrapper }}>
+            <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="flex-start"
+            >
+                    <Grid xs={12} item>
+                        <Typography variant='h2'>{props.recipe.name}</Typography>
+                    </Grid>
+                    <Grid item>
+                        <Typography>{props.recipe.ingredients}</Typography>
+                        <Typography>{props.recipe.instructions}</Typography>
+                        <Typography>{props.recipe.tags}</Typography>
+                    </Grid>
+                </Grid>
+            </Paper>
         </div>
     )
 };
